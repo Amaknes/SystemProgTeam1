c1aa77731bcdb1054335216ff883412a82d074c1
Valhys <pvince2010@hotmail.fr>
2018-12-06 17:14:25 +0100

    jkhgdfw

---
+++ ProjetProgrammationSystème/TestUnitairesProjetSystème/TestCuisine/TestControllerCuisine/TestStaff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestUnitairesProjetSystème.TestCuisine.TestControllerCuisine
+{
+    [TestClass]
+    public class TestStaff
+    {
+        [TestMethod]
+        public void TestSingleton()
+        {
+            //Arrange
+            Staff TestStaff1 = Staff.StaffInstance();
+            Staff TestStaff2 = Staff.StaffInstance();
+            Assert.AreSame(TestStaff1, TestStaff2);
+        }
+
+    }
+}
