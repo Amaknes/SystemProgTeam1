f0494e99e1aff4e126a6f497d638734cfcebf5fa
Amaknes <amaury.vincent@viacesi.fr>
2018-12-06 16:22:09 +0100

    First Test Passed

---
+++ ProjetProgrammationSystème/TestUnitairesProjetSystème/TestCusine/TestControllerCuisine/TestStaff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kitchen.Controller;
+
+namespace TestUnitairesProjetSystème.TestCusine.TestControllerCuisine
+{
+
+    [TestClass]
+    public class TestStaff
+    {
+        [TestMethod]
+        public void TestSingleton()
+        {
+            //Arrange
+            Staff TestStaff1 = Staff.StaffInstance();
+            Staff TestStaff2 = Staff.StaffInstance();
+            Assert.AreSame(TestStaff1, TestStaff2);
+        }
+
+    }
+}
